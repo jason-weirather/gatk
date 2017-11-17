2913edf7534a01783355ec06cdcc68f672262ff6
Jason L Weirather <jason-weirather@users.noreply.github.com>
2017-11-17 10:35:15 -0500

    add this

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p $PREFIX/jar
-cd $PREFIX/ && ./gradlew installDist
+cd $PREFIX/ && $PREFIX/gradlew installDist
 cp $SRC_DIR/build/libs/gatk.jar $PREFIX/jar/gatk.jar
 cp $SRC_DIR/bin/gatk $PREFIX/bin/gatk
