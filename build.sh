d432dbee6de16d90399014a5748655f0f5639305
Jason L Weirather <jason-weirather@users.noreply.github.com>
2017-11-20 12:29:54 -0500

    remove a big file

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 mkdir -p $PREFIX/jar
 cd $SRC_DIR/ && $SRC_DIR/gradlew
-cp $SRC_DIR/build/libs/gatk-4*-local.jar $PREFIX/jar/gatk.jar
+cp $SRC_DIR/build/libs/gatk-*-local.jar $PREFIX/jar/gatk.jar
 cp $SRC_DIR/bin/gatk $PREFIX/bin/gatk
+rm $SRC_DIR/build/gatk*.zip
