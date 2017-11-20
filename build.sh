9196d2f8d0ae25d4af437e8467fa03d772cdf6a9
Jason L Weirather <jason-weirather@users.noreply.github.com>
2017-11-20 10:09:28 -0500

    tick version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p $PREFIX/jar
-cd $SRC_DIR/ && $SRC_DIR/gradlew installDist
-cp $SRC_DIR/build/libs/gatk-4.beta.6-SNAPSHOT.jar $PREFIX/jar/gatk.jar
+cd $SRC_DIR/ && $SRC_DIR/gradlew
+cp $SRC_DIR/build/libs/gatk-4*-local.jar $PREFIX/jar/gatk.jar
 cp $SRC_DIR/bin/gatk $PREFIX/bin/gatk
